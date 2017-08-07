a7ae8a7eb0da58ed10b692ea96b247e5d170717e
sleahy115 <sleahy115@gmail.com>
2017-08-07 15:53:57 -0600

    output hero details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id:number;
+    name:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Sarah Leahy';
-  hero = 'Wonder Woman';
+  hero: Hero = {
+      id: 1,
+      name: "Wonder Woman"
+  };
 }
