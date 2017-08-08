d1bab6c9341cecef1d4790c91e95b0e4a0c92737
sleahy115 <sleahy115@gmail.com>
2017-08-07 18:50:00 -0600

    output list of heros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,18 @@ export class Hero {
     id:number;
     name:string;
 }
+const HEROS: Hero[] = [
+    {id:1, name:"Wonder Woman"},
+    {id:2, name: "Thor"},
+    {id:3, name: "Black Panther"},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'Sarah Leahy';
-  hero: Hero = {
-      id: 1,
-      name: "Wonder Woman"
-  };
+  heros = HEROS;
+  selectedHero: Hero;
 }
